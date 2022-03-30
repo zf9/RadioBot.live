dfe1e345818151061a987ee9b984bcdc94e3a65e
zf9 <51256315+zf9@users.noreply.github.com>
2022-03-30 13:14:01 +1300

    Add files via upload

---
+++ config/bot.js
@@ -0,0 +1,9 @@
+module.exports = {
+    discord: {
+        token: 'OT',
+        prefix: '$',
+        activity: '$help RadioBot.live | ZF9#1214 | ',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
